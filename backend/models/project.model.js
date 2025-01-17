1435c1bfcd8ebf4070965ed11834a064de4ee9d4
Hamaad Yousaf <108830855+HamaadYousaf@users.noreply.github.com>
2025-01-17 12:08:49 -0500

    project schema and get/post route

---
+++ backend/models/project.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+const ProjectSchema = mongoose.Schema({
+    project_name: { type: String, required: true },
+    description: { type: String },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'], // GeoJSON format for geolocation
+            required: true,
+        },
+        coordinates: { type: [Number], required: true }, // [longitude, latitude]
+    },
+    original_completion_date: { type: Date, required: true },
+    current_completion_date: { type: Date, required: true },
+    status: { type: String, enum: ['Planning', 'Under Construction', 'Completed'], required: true },
+    original_budget: { type: Number, required: true },
+    current_budget: { type: Number, required: true },
+    category: { type: String, enum: ['Transit', 'Communities', 'Roads/Bridges', 'Recreation', 'Health Care', 'Education'], required: true },
+    result: { type: String }, // Impact of project to the community
+    area: { type: String },
+    region: { type: String },
+    address: { type: String },
+    postal_code: { type: String },
+    municipal_funding: { type: Boolean },
+    provincial_funding: { type: Boolean },
+    federal_funding: { type: Boolean },
+    other_funding: { type: Boolean },
+    performance_metric: { type: Number },
+    efficiency: { type: String },
+}, {
+    timestamps: true // Adds `createdAt` and `updatedAt` fields
+})
+
+export const Project = mongoose.model('projects', ProjectSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose'
 import 'dotenv/config'
+import { Project } from './models/project.model.js';
 
 const app = express()
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,24 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.status(200).json({ data: "hello" })
 })
+
+app.post("/api/projects", async (req, res) => {
+    try {
+        const project = await Project.create(req.body)
+        res.status(200).json(project)
+    } catch (error) {
+        res.status(500).json({ msg: error.message })
+    }
+})
+
+app.get("/api/projects", async (req, res) => {
+    try {
+        const projects = await Project.find();
+        res.status(200).json(projects);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+});
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("Connected to MongoDB successfully")
