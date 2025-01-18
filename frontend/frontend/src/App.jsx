c28d3e0012be650f647c529125d22e345a0e3c2b
Zaid Khan <59452329+ZaidKhan02@users.noreply.github.com>
2025-01-18 14:24:24 -0500

    created files for frontend

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
 import './App.css'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Navbar from './Navbar'
+import Login from './Login'
+import Signup from './Signup'
+import Sidebar from './Sidebar/Sidebar'
+import DetailedPages from './DetailedPages'
+import ScrollToTop from './ScrollTop'
 
-function App() {
-
+export default function App() {
   return (
     <>
+
     </>
   )
 }
 
-export default App
+
