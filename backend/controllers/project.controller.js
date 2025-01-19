f075adb2fe6234166800fb07175e29d873ab2e29
Hamaad Yousaf <108830855+HamaadYousaf@users.noreply.github.com>
2025-01-19 16:10:45 -0500

    find project by id

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -9,6 +9,22 @@ export const getProjects = async (req, res) => {
     }
 }
 
+export const getProject = async (req, res) => {
+    try {
+        let id = req.params.id
+        const project = await Project.findById(id)
+
+        if (project == null) {
+            res.status(404).json({ msg: `project with id ${id} not found` });
+        }
+        else {
+            res.status(200).json(project);
+        }
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+}
+
 export const createProject = async (req, res) => {
     try {
         const project = await Project.create(req.body)
--- backend/routes/project.route.js
+++ backend/routes/project.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
 const router = express.Router();
-import { createProject, getProjects } from '../controllers/project.controller.js';
+import { createProject, getProjects, getProject } from '../controllers/project.controller.js';
 
 router.route('/').get(getProjects).post(createProject)
+router.route('/:id').get(getProject)
 
 export default router
