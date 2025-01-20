fd94969304ed0a19dae24b1319cf74479df8d455
Hamaad Yousaf <108830855+HamaadYousaf@users.noreply.github.com>
2025-01-20 11:15:57 -0500

    city summary

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -33,3 +33,28 @@ export const createProject = async (req, res) => {
         res.status(500).json({ msg: error.message })
     }
 }
+
+export const getSummary = async (req, res) => {
+    try {
+        const projects = await Project.find();
+        let performance = 0
+        let original_budget = 0
+        let current_budget = 0
+        for (const p of projects) {
+            performance += p.performance_metric
+            original_budget += p.original_budget
+            current_budget += p.current_budget
+        }
+
+        performance /= projects.length
+
+        res.status(200).json({
+            performance: performance,
+            original_budget: original_budget,
+            current_budget: current_budget,
+            budget_change: (current_budget - original_budget)
+        });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+}
--- backend/routes/project.route.js
+++ backend/routes/project.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router = express.Router();
-import { createProject, getProjects, getProject } from '../controllers/project.controller.js';
+import { createProject, getProjects, getProject, getSummary } from '../controllers/project.controller.js';
 
 router.route('/').get(getProjects).post(createProject)
+router.route('/summary').get(getSummary)
 router.route('/:id').get(getProject)
 
 export default router
