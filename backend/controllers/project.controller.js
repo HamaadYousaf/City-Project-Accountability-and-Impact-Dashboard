4f7dedd1f8c83b656134c3d694024d724a83243a
Hamaad Yousaf <108830855+HamaadYousaf@users.noreply.github.com>
2025-01-20 11:49:58 -0500

    city summary

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -40,19 +40,50 @@ export const getSummary = async (req, res) => {
         let performance = 0
         let original_budget = 0
         let current_budget = 0
+        let delays = 0
+
         for (const p of projects) {
             performance += p.performance_metric
             original_budget += p.original_budget
             current_budget += p.current_budget
+
+            const yearDiff = p.current_completion_date.getFullYear() - p.original_completion_date.getFullYear();
+            const monthDiff = p.current_completion_date.getMonth() - p.original_completion_date.getMonth();
+            const totalMonths = yearDiff * 12 + monthDiff;
+            delays += totalMonths
         }
 
         performance /= projects.length
+        delays /= projects.length
+        let budget_change = (current_budget - original_budget) / original_budget
+
+        const result = await Project.aggregate([
+            // Group by efficiency and count occurrences
+            {
+                $group: {
+                    _id: "$efficiency",
+                    count: { $sum: 1 }
+                }
+            },
+            // Sort by count in descending order
+            {
+                $sort: {
+                    count: -1
+                }
+            },
+            // Get only the first (most frequent) result
+            {
+                $limit: 1
+            }
+        ]);
 
         res.status(200).json({
             performance: performance,
             original_budget: original_budget,
             current_budget: current_budget,
-            budget_change: (current_budget - original_budget)
+            budget_change: budget_change,
+            delays: delays,
+            efficiency: result[0]._id
         });
     } catch (error) {
         res.status(500).json({ msg: error.message });
