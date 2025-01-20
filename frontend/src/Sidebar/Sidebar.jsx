34fd092046b22b2ccf88b6f0d13a9a6c07633de1
Zaid Khan <59452329+ZaidKhan02@users.noreply.github.com>
2025-01-20 11:15:13 -0500

    updated files

--- frontend/src/Sidebar/Sidebar.jsx
+++ frontend/src/Sidebar/Sidebar.jsx
@@ -8,17 +8,17 @@ import axios from "axios";
 
 
 export default function Sidebar() {
-    const [searchInput, setSearchInput] = useState("");
-    const [selectedOption, setSelectedOption] = useState("");
-    const [filteredProjects, setFilteredProjects] = useState([]);
-    const [projects, setProjects] = useState([]); // State for storing fetched project data
+    /* const [searchInput, setSearchInput] = useState("");
+     const [selectedOption, setSelectedOption] = useState("");
+     const [filteredProjects, setFilteredProjects] = useState([]); */
+    const [projects, setProjects] = useState([]);
 
     useEffect(() => {
         const fetchProjects = async () => {
             try {
                 const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/projects`);
-                setProjects(response.data);
-                setFilteredProjects(response.data);
+                setProjects(response.data.data);
+                //setFilteredProjects(response.data);
             } catch (error) {
                 console.error("Error fetching project data:", error);
             }
@@ -26,25 +26,25 @@ export default function Sidebar() {
         fetchProjects();
     }, []);
 
-    const handleRegionChange = (e) => {
+    /*const handleRegionChange = (e) => {
         const selectedRegion = e.target.value;
         setSelectedOption(selectedRegion);
         const filteredRegion = projects.filter(project => project.region === selectedRegion);
-        setFilteredProjects(filteredRegion); // Update the filtered projects state
+        setFilteredProjects(filteredRegion); 
     };
 
     const handleStatusChange = (e) => {
         const selectedStatus = e.target.value;
         setSelectedOption(selectedStatus);
         const filteredStatus = projects.filter(project => project.status === selectedStatus);
-        setFilteredProjects(filteredStatus); // Update the filtered projects state
+        setFilteredProjects(filteredStatus); 
     };
 
     const handleTypeChange = (e) => {
         const selectedType = e.target.value;
         setSelectedOption(selectedType);
         const filteredType = projects.filter(project => project.category === selectedType);
-        setFilteredProjects(filteredType); // Update the filtered projects state
+        setFilteredProjects(filteredType); 
     };
 
     const handleInputChange = (e) => {
@@ -52,7 +52,7 @@ export default function Sidebar() {
         setSearchInput(input);
         const filteredByInput = projects.filter(project => project.project_name.toLowerCase().startsWith(input));
         setFilteredProjects(filteredByInput);
-    }
+    }*/
 
     return (
         <>
@@ -60,20 +60,20 @@ export default function Sidebar() {
             <div className="inner-sidebar">
                 {/*
                 <div className="sidebar-logo">
-                    <h2 className="logo">LOGO</h2>
+                    <h2 className="logo">LOGO</h2> 
                 </div> */}
                 <input
                     type="text"
                     placeholder='Search for projects...'
                     className='search-box'
-                    value={searchInput}
-                    onChange={handleInputChange}
+                /*value={searchInput}
+                onChange={handleInputChange}*/
                 />
-                <Region handleChange={handleRegionChange} selectedOption={selectedOption} />
-                <Status handleChange={handleStatusChange} selectedOption={selectedOption} />
-                <Type handleChange={handleTypeChange} selectedOption={selectedOption} />
+                <Region />
+                <Status /*handleChange={handleStatusChange} selectedOption={selectedOption}*/ />
+                <Type /*handleChange={handleTypeChange} selectedOption={selectedOption}*/ />
             </div>
-            <Dashboard projects={filteredProjects} /> {/*filteredProjects comes from the useState above*/}
+            <Dashboard projects={projects} /> {/*filteredProjects comes from the useState above*/}
             {/*</div>*/}
         </>
     )
