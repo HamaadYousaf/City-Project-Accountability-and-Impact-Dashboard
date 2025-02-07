6748277c518be0bcf343c6f27392e12a3a19da4a
Hamaad Yousaf <108830855+HamaadYousaf@users.noreply.github.com>
2025-02-07 13:37:35 -0500

    schema fix

--- backend/routes/project.route.js
+++ backend/routes/project.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router();
-import { createProject, getProjects, getProject, getSummary, insertManyProjects } from '../controllers/project.controller.js';
+import { createProject, getProjects, getProject, getSummary, insertManyProjects, deleteAll } from '../controllers/project.controller.js';
 
 router.route('/').get(getProjects).post(createProject)
 router.route('/summary').get(getSummary)
 router.route('/:id').get(getProject)
 router.route('/insertMany').post(insertManyProjects)
+router.route('/deleteAll').delete(deleteAll)
 
 export default router
