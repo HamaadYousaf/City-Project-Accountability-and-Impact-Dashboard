6fbac13113c0ee1a058e386ef8723f434dba403b
Zaid Khan <59452329+ZaidKhan02@users.noreply.github.com>
2025-03-12 14:12:41 -0400

    added a map to each project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Navbar from './Navbar'
 import Sidebar from './Sidebar/Sidebar'
 import DetailedPages from './DetailedPages'
 import ScrollToTop from './ScrollTop'
+import 'leaflet/dist/leaflet.css';
+
 /*import dotenv from 'dotenv';
 
 dotenv.config();*/
