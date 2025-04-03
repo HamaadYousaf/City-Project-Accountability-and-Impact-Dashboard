4f97d9c9a2ad08546c7f6171e90dd4d8d6a73c11
Hamaad Yousaf <108830855+HamaadYousaf@users.noreply.github.com>
2025-04-03 19:20:59 -0400

    dockerized backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import projectRoutes from './routes/project.route.js'
 import userRoutes from './routes/user.route.js'
 import reportRoutes from './routes/report.route.js'
 import commentRoutes from './routes/comment.route.js'
-
+import morgan from 'morgan';
 
 
 const app = express()
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
-app.use(express.json({ limit: '50mb' })); 
+app.use(express.json({ limit: '50mb' }));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
     res.status(200).json({ data: "hello" })
